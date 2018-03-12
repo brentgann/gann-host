e97c92b1f5690871df14d08cda566e29216e082e
Brent <gann.brent@gmail.com>
2018-03-11 22:07:03 -0500

    Initial Commit

---
+++ src/main/java/me/gann/gannhost/GannHostApplication.java
@@ -0,0 +1,12 @@
+package me.gann.gannhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GannHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GannHostApplication.class, args);
+	}
+}
